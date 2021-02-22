8a56e8c96c1e3d794f537870547f38ebd35ea16c
donaldcrane <donaldagbakoba@gmail.com>
2021-02-22 12:23:36 +0100

    added calculator file

---
+++ calculator.php
@@ -0,0 +1,45 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$operator = $_POST['operator'];
+$result = '';
+
+if (is_numeric($num1) && is_numeric($num2)) {
+    switch ($operator) {
+        case "Add":
+           $result = $num1 + $num2;
+            break;
+        case "Subtract":
+           $result = $num1 - $num2;
+            break;
+        case "Multiply":
+            $result = $num1 * $num2;
+            break;
+        case "Divide":
+            $result = $num1 / $num2;
+    }
+}
+?>
+
+<html>
+    <body>
+        <div id="page-wrap">
+        <h1>Side Hustle - Simple Calculator Program</h1>
+        <form action="" method="post" id="quiz-for-task1">
+                <p>
+                    <input type="number" name="num1" id="num1" required="required" value="<?php echo $num1; ?>" /> <b>First Number</b>
+                </p>
+                <p>
+                    <input type="number" name="num2" id="num2" required="required" value="<?php echo $num2; ?>" /> <b>Second Number</b>
+                </p>
+                <p>
+                    <input readonly="readonly" name="result" value="<?php echo $result; ?>"> <b>Result</b>
+                </p>
+                <input type="submit" name="operator" value="Add" />
+                <input type="submit" name="operator" value="Subtract" />
+                <input type="submit" name="operator" value="Multiply" />
+                <input type="submit" name="operator" value="Divide" />
+        </form>
+        </div>
+    </body>
+</html>
